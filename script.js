accac5fa5e98f3150ad15680a553e777df3593b5
Akansha1780 <123267037+Akansha1780@users.noreply.github.com>
2026-02-26 00:46:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log("Akansha Cloud Portfolio Loaded Successfully!");
+const text = ["Cloud Enthusiast", "Software Developer", "Future Data Scientist"];
+let i = 0, j = 0, current = "", isDeleting = false;
+const typing = document.querySelector(".typing");
+
+function type(){
+  if(i < text.length){
+    if(!isDeleting && j <= text[i].length){
+      current = text[i].slice(0, j++);
+    } else if(isDeleting && j >= 0){
+      current = text[i].slice(0, j--);
+    }
+
+    typing.innerHTML = current;
+
+    if(j === text[i].length){
+      isDeleting = true;
+    }
+    if(isDeleting && j === 0){
+      isDeleting = false;
+      i++;
+    }
+    if(i === text.length) i = 0;
+  }
+  setTimeout(type, isDeleting ? 80 : 120);
+}
+type();
